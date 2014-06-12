c108b5db78a58e5f3de9b08389f5109d2b945297
Grashalm <christian.humer@gmail.com>
2014-06-12 14:47:03 +0200

    Added basic template; implemented login

---
+++ teacher/app/js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('learntoprogram', [
+  'ngRoute',
+  'learntoprogram.filters',
+  'learntoprogram.services',
+  'learntoprogram.directives',
+  'learntoprogram.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/single', {templateUrl: 'partials/single.html', controller: 'Single'});
+  $routeProvider.when('/multi', {templateUrl: 'partials/multi.html', controller: 'Multi'});
+  $routeProvider.when('/teacher', {templateUrl: 'partials/teacher.html', controller: 'Teacher'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'Login'});
+  $routeProvider.otherwise({redirectTo: '/login'});
+}]);
---
+++ teacher/app/js/controllers.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('learntoprogram.controllers', [])
+    .controller('Teacher', ['$scope', function($scope) {
+
+    }])
+    .controller('Single', ['$scope', function($scope) {
+
+    }])
+    .controller('Multi', ['$scope', function($scope) {
+
+    }])
+    .controller('Login', function ($scope, $rootScope, $location) {
+        $scope.credentials = {
+            username: '',
+            password: ''
+        };
+        $scope.loginError = '',
+        $scope.login = function (credentials) {
+            if (credentials.username == 'single' || credentials.username == 'multi' || credentials.username == 'teacher') { // test
+                $rootScope.loggedUser = credentials.username;
+                $location.path( "/" + credentials.username );
+            } else {
+                $scope.loginError = "Invalid user/pass.";
+            }
+        };
+    });
